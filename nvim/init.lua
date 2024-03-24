8af14fb9622a0f82b8ffaec5ef9f39679cd96319
knownasrares <known.as.rares@gmail.com>
2024-03-24 02:51:44 +0200

    improved nvim cfg and deleted useless files

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,23 +17,49 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Lazy plugins
 require("lazy").setup({
+	{'nvim-treesitter/nvim-treesitter'},
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
 	{'neovim/nvim-lspconfig'},
 	{'hrsh7th/nvim-cmp'},
 	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/cmp-buffer'},
 	{'L3MON4D3/LuaSnip'},
-	{'windwp/nvim-autopairs',
+	{
+		'windwp/nvim-autopairs',
 		event = "InsertEnter",
 		config = true
 		-- use opts = {} for passing setup options
 		-- this is equalent to setup({}) function
+	},
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		dependencies = { 'nvim-lua/plenary.nvim' }
 	}
 })
 
+-------------------------------------------------------------------------------
+
 -- Editor settings
 vim.wo.number = true
+vim.cmd('autocmd! FileType * setlocal signcolumn=no')
+
+-- Define the highlight group for floating windows
+vim.cmd [[highlight FloatBorder guifg=#ffffff guibg=#4c566a]] -- Border color
+vim.cmd [[highlight NormalFloat guifg=#d8dee9 guibg=#2e3440]] -- Content area color
+
+-- Set the background color of the entire floating window
+vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#2e3440]]
+
+-- Additional adjustments if needed
+vim.cmd [[autocmd ColorScheme * highlight FloatBorder guibg=#4c566a]]
+
+-- Set LSPZero's autocomplete prompt background color
+vim.cmd [[autocmd ColorScheme * hi LspFloatWinBorder guifg=#ffffff guibg=#4c566a]] -- Border color
+vim.cmd [[autocmd ColorScheme * hi LspFloatWinNormal guifg=#d8dee9 guibg=#2e3440]] -- Content area color
+
+-------------------------------------------------------------------------------
 
 -- Plugins config
 local lsp_zero = require('lsp-zero')
@@ -53,12 +79,19 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
+	sources = {
+		{name = 'nvim_lsp'},
+		{name = 'nvim_lua'},
+	},
 	mapping = cmp.mapping.preset.insert({
+		-- Super-Tab to go from snip to snip
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+    		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
 		-- `Enter` key to confirm completion
 		['<CR>'] = cmp.mapping.confirm({select = true}),
 
 		-- Ctrl+Space to trigger completion menu
-		['<C-Space>'] = cmp.mapping.complete(),
+		-- ['<C-Space>'] = cmp.mapping.complete(),
 
 		-- Navigate between snippet placeholder
 		['<C-f>'] = cmp_action.luasnip_jump_forward(),
@@ -67,5 +100,24 @@ cmp.setup({
 		-- Scroll up and down in the completion documentation
 		['<C-u>'] = cmp.mapping.scroll_docs(-4),
 		['<C-d>'] = cmp.mapping.scroll_docs(4),
-	})
+	}),
+	window = {
+		-- Makes the autocompletion menu prettier
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = {'abbr', 'menu', 'kind'},
+		format = function(entry, item)
+			local short_name = {
+				nvim_lsp = 'LSP',
+				nvim_lua = 'nvim'
+			}
+
+			local menu_name = short_name[entry.source.name] or entry.source.name
+
+			item.menu = string.format('[%s]', menu_name)
+			return item
+		end,
+	},
 })
